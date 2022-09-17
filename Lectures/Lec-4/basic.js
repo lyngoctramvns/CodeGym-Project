7302a04a799fe25bce6211178939fdb450c9c96f
mimiudoublemilk <mimiudoublemilk@gmail.com>
2022-09-17 22:02:33 +0700

    Lec 4

---
+++ Lectures/Lec-4/basic.js
@@ -0,0 +1,8 @@
+const numberFirst = +prompt("Nhập số thứ 1");
+const numberSecond = +prompt("Nhập số thứ 2");
+
+let sumNumber = numberFirst + numberSecond;
+sumNumber++; //Cộng thêm 1 vào sum
+sumNumber += 6 //sumNumber = sum + 6
+
+document.write(sumNumber);
