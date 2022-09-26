e69dd2bce55cbce372cdeef227eebfb1a2d49820
mimiudoublemilk <mimiudoublemilk@gmail.com>
2022-09-26 12:40:57 +0700

    letter

---
+++ Exercises/Javascript/Work-10/letter.js
@@ -0,0 +1,21 @@
+function letter (){
+    let name = document.getElementById('name').value;
+    let place = document.getElementById('place').value;
+    let date = document.getElementById('date').value;
+
+    document.write(
+
+        name + " thương nhớ," + "<br>"
+        + "<br>" +
+        "Em không biết phải nói sao để anh hiểu rằng, em nhớ anh thật nhiều. Em yêu anh đến khi trái tim này tan thành nghìn mảnh. Tất" + "<br>" + "cả những gì em yêu thương, em khát khao và cần đến, chính là anh, mãi mãi về sau. Em chỉ muốn ở bên anh, và anh yêu hỡi," + "<br>" + "em sẽ trở thành người phụ nữ như anh mong muốn." + "<br>"
+        + "<br>" +
+
+        "Có phải em thật tệ hại, khi cứ nghĩ đến anh thật nhiều, thật lâu và nhất là mỗi khi đêm xuống? Em hứa sẽ sẽ cố gắng triệu triệu" + "<br>" + "lần hơn thế. Nhưng hơn tất cả, em chỉ mong một ngày nào đó, anh sẽ tự hào về em, như tự hào về vợ của anh, và mẹ của các" + "<br>" + "con anh (ít nhất là 2 nhé, em vừa mới quyết định đấy!). Em nhớ thật nhiều cảm giác mỗi đêm anh ôm em và ru em ngủ trong" + "<br>" + "vòng tay. Đêm nay, em chỉ muốn được gần bên anh… và anh biết không, trái tim em đang đau đớn đến nhường nào." + "<br>"
+        + "<br>" +
+
+        "Anh yêu thương, đừng bao giờ rời xa em nữa nhé. Yêu anh rất nhiều." + "<br>"
+        + "<br>"
+        + place + ", " + date + "."
+    );
+
+};
