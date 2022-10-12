c72a3d6ecdaaaeb1543165f483dda4146b715c84
mimiudoublemilk <mimiudoublemilk@gmail.com>
2022-10-12 08:52:41 +0700

    work-3

--- Exercises/Else-if/Work-3/basic.js
+++ Exercises/Else-if/Work-3/basic.js
@@ -1,1 +1,14 @@
-alert('Hello!');
+//Bài 1
+switch(browser) {
+    case Edge:
+        alert("You've got the Edge!");
+        break;
+    case Chrome:
+    case Firefox:
+    case Safari:
+    case Opera:
+        alert('Okay we support these browsers too');
+        break;
+    default:
+        alert('We hope that this page looks ok!');
+};
