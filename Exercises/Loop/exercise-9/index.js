d20442abca1f88c5675dd6f4b28dac0088c7cf41
lyngoctramvns <lyngoctramvns@gmail.com>
2022-11-20 19:18:01 +0700

    triangle

--- Exercises/Loop/exercise-9/index.js
+++ Exercises/Loop/exercise-9/index.js
@@ -1,10 +1,34 @@
 function triangleAsc(){
-  let i = 0;
-  for (i=0;i<6;i++){
-    document.getElementById('asc').innerHTML = 
+
+  let row = "";
+
+  for(i=0;i< 5;i++){
+    let newrow = row + "*"
+    row = newrow
+    document.write(row);
+    document.write('</br>');
   }
 }
 
+
 function triangleDecs(){
 
+  let row = "******"
+
+  for (let i=0;i<5;i++){
+    let newrow = row.replace("*","");
+    row = newrow;
+    document.write(row);
+    document.write('</br>');
+  }
+
 }
+
+
+triangleAsc();
+document.write("</br>");
+triangleDecs();
+document.write("</br>");
+triangleAsc();
+document.write("</br>");
+triangleDecs();
